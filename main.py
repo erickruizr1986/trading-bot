6c9a787a2f402c7043141f60a5c9db902cabc474
erickruizr1986 <erickruizr@gmail.com>
2026-04-20 10:43:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,6 @@ def trend_5m_proxy(df):
 def signal_loop():
     global last_trade_time
     send("🎯 PRECISION BOT ACTIVO")
-    send("BOT VIVO Y MONITOREANDO 📡")
     
     while True:
         try:
